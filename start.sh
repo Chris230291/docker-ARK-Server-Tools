fec4e4b13e980bf0f5cefa2d008e65fc04940d31
Chris230291 <Chris.Gironi@me.com>
2019-08-29 18:38:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Make sure steam owns these directories
-sudo chown steam:steam -R /etc/arkmanager /home/steam/ARK /etc/arkmanager/instances/
+#sudo chown steam:steam -R /etc/arkmanager /home/steam/ARK
 
 # Create base config if it doesnt exist
 if [[ ! -e /etc/arkmanager/arkmanager.cfg ]]
 then
-echo "Creating 'arkmanager.cfg'" && curl -s -o /etc/arkmanager/arkmanager.cfg https://raw.githubusercontent.com/FezVrasta/ark-server-tools/master/tools/arkmanager.cfg && sed -i -e 's!ARK-Staging!ARK/ARK-Staging!g' -e 's!ARK-Backups!ARK/ARK-Backups!g' /etc/arkmanager/arkmanager.cfg
+echo "Creating 'arkmanager.cfg'"
+sudo curl -s -o /etc/arkmanager/arkmanager.cfg https://raw.githubusercontent.com/FezVrasta/ark-server-tools/master/tools/arkmanager.cfg
+sudo sed -i -e 's!ARK-Staging!ARK/ARK-Staging!g' -e 's!ARK-Backups!ARK/ARK-Backups!g' /etc/arkmanager/arkmanager.cfg
 fi
 
 # Create instance example if it doesnt exist
 if [[ ! -e /etc/arkmanager/instances/instance.cfg.example ]]
 then
-echo "Creating 'instance.cfg.example'" && curl -s --create-dirs -o /etc/arkmanager/instances/instance.cfg.example https://raw.githubusercontent.com/FezVrasta/ark-server-tools/master/tools/instance.cfg.example
+echo "Creating 'instance.cfg.example'"
+sudo curl -s --create-dirs -o /etc/arkmanager/instances/instance.cfg.example https://raw.githubusercontent.com/FezVrasta/ark-server-tools/master/tools/instance.cfg.example
 fi
 
 if [ ${UPDATETOOLS} -eq 1 ]
